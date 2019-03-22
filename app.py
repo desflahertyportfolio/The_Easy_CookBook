cbc6328267b606a2e4ba5421defc650b8e09d104
Des Flaherty <desflaherty@hotmail.com>
2019-03-22 19:32:03 +0000

    added cuisine file to test

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] ='CookBook'
+app.config["MONGO_URI"] ='mongodb+srv://root:Allergan99@myfirstcluster-lgqe5.mongodb.net/CookBook'
+
+"""
+app.debug = False
+if app.debug == True:
+    import config
+    app.config["MONGO_DBNAME"] = config.DB_CONFIG['MONGO_DBNAME']
+    app.config["MONGO_URI"] = config.DB_CONFIG['MONGO_URI']
+else:
+    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+"""
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello world'
-    
+@app.route('/get_cuisine')
+def get_cuisine():
+    return render_template("cuisine.html", cuisine=mongo.db.Cuisine.find())
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
