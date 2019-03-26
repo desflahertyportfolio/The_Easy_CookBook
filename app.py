7eb27504073fe2deb68dd45824796df92fafd051
Des Flaherty <desflaherty@hotmail.com>
2019-03-26 13:00:29 +0000

    adding edit recipe

--- app.py
+++ app.py
@@ -29,15 +29,16 @@ mongo = PyMongo(app)
 def home():
     return render_template("index.html")
 
-@app.route('/recipes')
-def recipes():
-    return render_template("recipes.html")
+
    
 @app.route('/cuisine')
 def cuisine():
     return render_template("cuisine.html", cuisine=mongo.db.Cuisine.find())    
+ 
     
-    
+@app.route('/recipes')
+def recipes():
+    return render_template("recipes.html")    
     
     
 @app.route('/add_recipes')
@@ -50,11 +51,6 @@ def add_recipes():
     skill=mongo.db.Skill.find()
     )
    
-  
-    
-    
-    
-    
     
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -62,6 +58,50 @@ def insert_recipe():
     recipe.insert_one(request.form.to_dict())
     return redirect(url_for('recipes'))    
     
+    
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.Recipe.find_one({"_id": ObjectId(recipe_id)})
+    all_cuisines =  mongo.db.Cuisine.find()
+    all_courses =  mongo.db.Course.find()
+    all_occasions =  mongo.db.Occasion.find()
+    all_diets =  mongo.db.Special_Diets.find()
+    all_skills =  mongo.db.Skill.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           cuisne=all_cuisines,course=all_courses,
+                           occasion=all_occasions,diets=all_diets,skill=all_skills)
+                           
+                           
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipe = mongo.db.Recipe
+    recipe.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name':request.form.get('recipe_name'),
+        'cuisine_name':request.form.get('cuisine_name'),
+        'course':request.form.get('course'),
+        'occasion':request.form.get('occasion'),
+        'special_diets':request.form.get('special_diets'),
+        'skill_level':request.form.get('skill_level'),
+        'ingredients':request.form.get('ingredients'),
+        'cook_time':request.form.get('cook_time'),
+        'prep_time':request.form.get('prep_time'),
+        'serves':request.form.get('serves'),
+        'skill_level':request.form.get('skill_level'),
+        'instruction_step_1':request.form.get('instruction_step_1'),
+        'instruction_step_2':request.form.get('instruction_step_2'),
+        'instruction_step_3':request.form.get('instruction_step_3'),
+        'instruction_step_4':request.form.get('instruction_step_4'),
+        'instruction_step_5':request.form.get('instruction_step_5'),
+        'instruction_step_6':request.form.get('instruction_step_6')
+    })
+    return redirect(url_for('recipe'))  
+    
+
+@app.route('/delete_recipe/<recipe_id>', methods=["POST"])
+def delete_recipe(recipe_id):
+    mongo.db.Recipe.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('recipe'))    
  
 
 
