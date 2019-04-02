372a46cb991d788e32da23914b9735dc853ad062
Des Flaherty <desflaherty@hotmail.com>
2019-04-02 16:27:13 +0000

    trying to add a reset for dropdown

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -1,8 +1,9 @@
  $(document).ready(function() {
+  
             $('.collapsible').collapsible();
             $('select').material_select();
             $(".button-collapse").sideNav();
-            
-        });
+
+ });
         
-        
+
