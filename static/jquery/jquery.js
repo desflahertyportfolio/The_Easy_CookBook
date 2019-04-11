d15b787d1a62ef97224eddd2655848bf03da49a3
Des Flaherty <desflaherty@hotmail.com>
2019-04-11 13:41:51 +0000

    added a modal pop up for delete confirmation

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -3,17 +3,10 @@
             $('.collapsible').collapsible();
             $('select').material_select();
             $(".button-collapse").sideNav();
-             $('.modal').modal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      inDuration: 300, // Transition in duration
-      outDuration: 200, // Transition out duration
-      startingTop: '4%', // Starting top style attribute
-      endingTop: '10%', // Ending top style attribute
-    }
-            );
-            $('.modal-trigger').modal();
-            $('#modal1').find("#image_url").val('asd');
+            $('#modal1').modal();
+            
  });
         
 
+
+    
