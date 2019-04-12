e3342e7ece1bf3070fa9e00ec5e742b9aa40008b
Des Flaherty <desflaherty@hotmail.com>
2019-04-12 10:59:23 +0000

    add pw form control to login form

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -4,8 +4,28 @@
             $('select').material_select();//form creation
             $(".button-collapse").sideNav();//side nav for mobile view
             $('#modal1').modal();//Delete message confirmation popup
-            // Form validation for dropdown menus
+            // HTML Form validation for dropdown menus
             $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute'}); 
+            
+       //validate passwords match on login
+       $("#password").on("focusout", function (e) {
+        if ($(this).val() != $("#password1").val()) {
+        $("#password1").removeClass("valid").addClass("invalid");
+        } else {
+        $("#password1").removeClass("invalid").addClass("valid");
+        }
+        });
+
+       $("#password1").on("keyup", function (e) {
+      if ($("#password").val() != $(this).val()) {
+        $(this).removeClass("valid").addClass("invalid");
+     } else {
+        $(this).removeClass("invalid").addClass("valid");
+     }
+     });
+            
+            
+            
  });
         
 
