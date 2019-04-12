cc28647a458ba5ba1bdabe2e942d39cbe19531ff
Des Flaherty <desflaherty@hotmail.com>
2019-04-12 10:20:50 +0000

    add form control for drop dowm menus

--- static/jquery/jquery.js
+++ static/jquery/jquery.js
@@ -1,12 +1,13 @@
  $(document).ready(function() {
   
             $('.collapsible').collapsible();
-            $('select').material_select();
-            $(".button-collapse").sideNav();
-            $('#modal1').modal();
-            
-         
+            $('select').material_select();//form creation
+            $(".button-collapse").sideNav();//side nav for mobile view
+            $('#modal1').modal();//Delete message confirmation popup
+            // Form validation for dropdown menus
+            $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute'}); 
  });
         
 
- 
+
+     
