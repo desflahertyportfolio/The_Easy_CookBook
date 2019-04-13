ef70a3d5de0953a7c53919a0196baa18beeb9365
Des Flaherty <desflaherty@hotmail.com>
2019-04-13 14:42:01 +0000

    config file

--- app.py
+++ app.py
@@ -5,15 +5,31 @@ import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from bson.objectid import ObjectId
-import config
+
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] ='CookBook'
+app.config["MONGO_URI"] ='mongodb+srv://root:Allergan99@myfirstcluster-lgqe5.mongodb.net/CookBook'
 
- 
-app.secret_key = config.DB_CONFIG['SECRET_KEY']
-app.config["MONGO_DBNAME"] = config.DB_CONFIG['MONGO_DBNAME']
-app.config["MONGO_URI"] = config.DB_CONFIG['MONGO_URI']
+#app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY']="\xd4\xf3}gi\xa8fK\x87`\xbc\xea\xc5R\x81\xc1Ho\xba'\x85\xd5$\xf4"
+
+"""
+app.debug = False
+if app.debug == True:
+    import config
+    app.config["MONGO_DBNAME"] = config.DB_CONFIG['MONGO_DBNAME']
+    app.config["MONGO_URI"] = config.DB_CONFIG['MONGO_URI']
+else:
+    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+"""
+
+
+
+
 
 
 
