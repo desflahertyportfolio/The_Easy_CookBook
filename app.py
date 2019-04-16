184927924522e471a6e4a914a68597c46f91bbc8
Des Flaherty <desflaherty@hotmail.com>
2019-04-16 12:16:18 +0000

    set debug to false

--- app.py
+++ app.py
@@ -353,5 +353,5 @@ def find_multiple_categories():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
             
